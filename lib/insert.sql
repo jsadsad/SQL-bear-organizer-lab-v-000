2271f16bad0ccaf9569407c8e8da599fefcfa014
Joshua Clarence Sadsad <joshsad2@gmail.com>
2018-11-07 22:38:08 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO bears (name) VALUES("Mr. Chocolate");
+INSERT INTO bears (name) VALUES();
+INSERT INTO bears (name) VALUES();
+INSERT INTO bears (name) VALUES();
+INSERT INTO bears (name) VALUES();
+INSERT INTO bears (name) VALUES();
+INSERT INTO bears (name) VALUES();
